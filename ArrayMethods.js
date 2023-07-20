c48fab9565f51a9e6d8535ea217a6cf25fa9169c
Sujit Sable <87574722+devil1229@users.noreply.github.com>
2023-07-20 23:07:37 +0530

    Create ArrayMethods.js

---
+++ ArrayMethods.js
@@ -0,0 +1,14 @@
+//array Methods
+
+//map
+
+//syntax
+var new_array = arr.map(function callback(element, index, array) {
+    // Return value for new_array
+}[, thisArg])
+
+//examples
+
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(item => item * 2);
+console.log(doubled); // [2, 4, 6, 8]
